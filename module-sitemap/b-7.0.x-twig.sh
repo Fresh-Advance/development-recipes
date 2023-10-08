b6d766fe10d8f372e9aa82718b3f31a61700b600
Anton Fedurtsya <anton@fedurtsya.com>
2023-10-08 14:59:24 +0300

    Add sitemap module recipe
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- module-sitemap/b-7.0.x-twig.sh
+++ module-sitemap/b-7.0.x-twig.sh
@@ -18,18 +18,11 @@ $SCRIPT_PATH/../parts/shared/prepare_shop_package.sh -e"CE" -b"b-7.0.x"
 $SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"CE" -b"b-7.0.x" -t"twig"
 $SCRIPT_PATH/../parts/shared/require_theme.sh -t"twig" -b"b-7.0.x"
 
-git clone https://github.com/Fresh-Advance/Invoice source/dev-packages/invoice
+git clone https://github.com/Fresh-Advance/Sitemap source/dev-packages/sitemap
 docker-compose exec \
-  php composer config repositories.fresh-advance/invoice \
-  --json '{"type":"path", "url":"./dev-packages/invoice", "options": {"symlink": true}}'
-docker-compose exec php composer require fresh-advance/invoice:* --no-update
-
-# Configure codeception runner
-perl -pi\
-  -e 's#theme_id: .*#theme_id: twig#g;'\
-  -e 's#admin_smarty#admin_twig#g;'\
-  -e 's#\/flow\/translations\/#\/twig\/#g;'\
-  source/dev-packages/invoice/tests/Codeception/acceptance.suite.yml
+  php composer config repositories.fresh-advance/sitemap \
+  --json '{"type":"path", "url":"./dev-packages/sitemap", "options": {"symlink": true}}'
+docker-compose exec php composer require fresh-advance/sitemap:* --no-update
 
 $SCRIPT_PATH/../parts/shared/require_demodata_package.sh -e"CE" -b"b-7.0.x"
 
@@ -38,7 +31,7 @@ docker-compose exec -T php composer update --no-interaction
 
 $SCRIPT_PATH/../parts/shared/setup_database.sh
 
-docker-compose exec -T php bin/oe-console oe:module:activate fa_invoice
+docker-compose exec -T php bin/oe-console oe:module:activate fa_sitemap
 docker-compose exec -T php bin/oe-console oe:theme:activate twig
 
 $SCRIPT_PATH/../parts/shared/create_admin.sh
