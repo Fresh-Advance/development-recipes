257365947321ccadda6c7445c90c1628d229b613
Anton Fedurtsya <anton@fedurtsya.com>
2024-05-11 14:10:12 +0300

    Use compilation style installation in recipes
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- module-sitemap/b-7.0.x-twig.sh
+++ module-sitemap/b-7.0.x-twig.sh
@@ -14,26 +14,42 @@ perl -pi\
   -e 's#error_reporting = .*#error_reporting = E_ALL ^ E_WARNING ^ E_DEPRECATED#g;'\
   containers/php-fpm/custom.ini
 
-$SCRIPT_PATH/../parts/shared/prepare_shop_package.sh -e"CE" -b"b-7.0.x"
-$SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"CE" -b"b-7.0.x"
-$SCRIPT_PATH/../parts/shared/require_theme.sh -t"twig" -b"b-7.0.x"
+perl -pi\
+  -e 's#/var/www/#/var/www/source/#g;'\
+  containers/httpd/project.conf
 
-git clone https://github.com/Fresh-Advance/Sitemap source/dev-packages/sitemap
-docker compose exec \
-  php composer config repositories.fresh-advance/sitemap \
-  --json '{"type":"path", "url":"./dev-packages/sitemap", "options": {"symlink": true}}'
-docker compose exec php composer require fresh-advance/sitemap:* --no-update
+perl -pi\
+  -e 's#PHP_VERSION=.*#PHP_VERSION=8.1#g;'\
+  .env
+
+mkdir source
+docker compose up --build -d php
+
+cp ${SCRIPT_PATH}/../parts/bases/composer.json.base ./source/composer.json
+
+$SCRIPT_PATH/../parts/shared/require.sh -n"oxid-esales/oxideshop-ce" -g"https://github.com/OXID-eSales/oxideshop_ce.git" -v"dev-b-7.0.x"
+$SCRIPT_PATH/../parts/shared/require.sh -n"oxid-esales/developer-tools" -v"dev-b-7.0.x"
+$SCRIPT_PATH/../parts/shared/require.sh -n"fresh-advance/sitemap" -g"https://github.com/Fresh-Advance/Sitemap.git" -v"dev-b-7.0.x"
+
+$SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"CE" -b"b-7.0.x"
+
+$SCRIPT_PATH/../parts/shared/require.sh -n"oxid-esales/twig-theme" -v"dev-b-7.0.x"
 
 $SCRIPT_PATH/../parts/shared/require_demodata_package.sh -e"CE" -b"b-7.0.x"
 
-# Install all preconfigured dependencies
-docker compose exec -T php composer update --no-interaction
+docker compose exec php composer update --no-interaction
+
+make up
 
 $SCRIPT_PATH/../parts/shared/setup_database.sh
 
-docker compose exec -T php bin/oe-console oe:module:activate fa_sitemap
-docker compose exec -T php bin/oe-console oe:theme:activate twig
+docker compose exec -T php vendor/bin/oe-console oe:module:activate fa_sitemap
+docker compose exec -T php vendor/bin/oe-console oe:theme:activate twig
 
 $SCRIPT_PATH/../parts/shared/create_admin.sh
 
-echo "Done!"
+# Register all related project packages git repositories
+mkdir -p .idea; cp "${SCRIPT_PATH}/../parts/bases/vcs.xml.base" .idea/vcs.xml
+perl -pi\
+  -e 's#</component>#<mapping directory="\$PROJECT_DIR\$/source/vendor/fresh-advance/sitemap" vcs="Git" />\n  </component>#g;'\
+  .idea/vcs.xml
